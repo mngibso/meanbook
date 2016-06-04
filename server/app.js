96e5ff69813ed622f9a411bbd2b062b58f514f3b
mngibso <mark@gibsonsoftware.com>
2016-06-03 21:45:30 -0600

    restructure

--- server/app.js
+++ server/app.js
@@ -20,7 +20,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+//Refer to public outside of current directory
+app.use(express.static(path.normalize( path.join(__dirname, '../public'))));
 
 app.use('/', routes);
 app.use('/users', users);
